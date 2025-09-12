b0cde9672d4e5079a6b727dacb7f4a469150630a
Praj2003 <prajval.kanda01@gmail.com>
2025-09-12 07:43:36 +0530

    Worked on Category show page

---
+++ app/categoryShow/page.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import ColourfulText from "@/components/ui/colourful-text";
+import Image from "next/image";
+
+const CategoryShowPage = () => {
+  return (
+    <div className="min-w-full min-h-screen flex justify-center items-center">
+      <div className="container flex flex-col items-center justify-center gap-10 lg:gap-16 mt-20">
+        <h1 className="text-white font-bold lg:text-7xl md:text-6xl text-4xl">
+          One Stop, <ColourfulText text="Endless" /> Choices
+        </h1>
+
+        <div className="flex items-center justify-center flex-wrap gap-10">
+          <div className="flex flex-col items-center justify-center">
+            <div className="lg:w-[200px] md:w-[150px] w-[150px] lg:h-[200px] md:h-[150px] h-[150px] bg-white rounded-full relative cursor-pointer">
+              <Image src={"/images/ClothingTrezy.jpg"} fill={true} alt="Clothing Category" className="rounded-full"/>
+            </div>
+            <p className="text-white font-semibold mt-2 text-2xl">Clothing</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className="lg:w-[200px] md:w-[150px] w-[150px] lg:h-[200px] md:h-[150px] h-[150px] bg-white rounded-full relative cursor-pointer">
+                 <Image src={"/images/electronicsTrezy.jpg"} fill={true} alt="Clothing Category" className="rounded-full"/>
+            </div>
+            <p className="text-white font-semibold mt-2 text-2xl">
+              Electronics
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className="lg:w-[200px] md:w-[150px] w-[150px] lg:h-[200px] md:h-[150px] h-[150px] bg-white rounded-full relative cursor-pointer">
+                 <Image src={"/images/gamingTrezy.jpg"} fill={true} alt="Clothing Category" className="rounded-full"/>
+            </div>
+            <p className="text-white font-semibold mt-2 text-2xl">Gaming</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <div className="lg:w-[200px] md:w-[150px] w-[150px] lg:h-[200px] md:h-[150px] h-[150px] bg-white rounded-full relative cursor-pointer">
+                 <Image src={"/images/essentialsTrezy.jpg"} fill={true} alt="Clothing Category" className="rounded-full"/>
+            </div>
+            <p className="text-white font-semibold mt-2 text-2xl">Essentials</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryShowPage;
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="min-w-full min-h-screen bg-slate-900 grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1">
       <div className="w-full flex items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-6">
-          <h1 className="lg:text-5xl text-4xl text-blue-500 font-bold text-center">
+          <h1 className="lg:text-5xl text-4xl text-amber-500 font-bold text-center">
             Shopping made{" "}
             <span className="inline-block w-[140px] text-left ">
               <FlipWords words={words} />
@@ -21,23 +21,23 @@ export default function Home() {
           </h1>
 
           <div className="flex items-center justify-center lg:gap-8 md:gap-1 gap-2 mb-6 mt-5">
-            <div className="flex items-center gap-2 text-teal-700 font-bold">
-              <FaWallet className="text-teal-700" />
+            <div className="flex items-center gap-2 text-blue-400 font-bold">
+              <FaWallet className="text-white" />
               Secure Payments
             </div>
 
-            <div className="flex items-center gap-2 text-teal-700 font-bold">
-              <MdOutlineShoppingCart className="text-teal-700" />
+            <div className="flex items-center gap-2 text-blue-400 font-bold">
+              <MdOutlineShoppingCart className="text-white" />
               Latest Products
             </div>
 
-            <div className="flex items-center gap-2 text-teal-700 font-bold">
-              <MdSupportAgent className="text-teal-700" />
+            <div className="flex items-center gap-2 text-blue-400 font-bold">
+              <MdSupportAgent className="text-white" />
               Reliable Support
             </div>
           </div>
 
-          <div className="flex items-center justify-center gap-10 mt-4">
+          <div className="flex items-center justify-center gap-10 mt-10">
             <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
@@ -57,9 +57,9 @@ export default function Home() {
 
       <div className="Picture w-full lg:flex items-center justify-center md:hidden hidden">
         <CardContainer className="mt-10">
-          <CardBody className="w-[500px] h-[500px] border-3  border-amber-600 shadow-2xl flex flex-col items-center justify-center gap-6">
+          <CardBody className="w-[500px] h-[500px] border-3 bg-white  border-amber-600 shadow-2xl flex flex-col items-center justify-center gap-6">
             <CardItem translateZ={20}>
-              <h1 className=" font-bold text-2xl bg-gradient-to-r from-amber-500 via-amber-500 to-white bg-clip-text text-transparent">
+              <h1 className=" font-bold text-2xl bg-gradient-to-r from-amber-500 via-amber-500 to-amber-600 bg-clip-text text-transparent">
                 Your cart been waiting for you at TrezyKart
               </h1>
             </CardItem>
