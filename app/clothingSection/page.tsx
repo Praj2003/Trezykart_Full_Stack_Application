2b7aee30878ad4e7d2173c5df8131de52c3c185c
Praj2003 <prajval.kanda01@gmail.com>
2025-09-20 10:18:53 +0530

    Working on Card to showcase product fetures and pricing

--- app/clothingSection/page.tsx
+++ app/clothingSection/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
 import ClothingDisplay from "@/components/ClothingDisplay";
+import clothingData from "@/dataSets/ClothingData";
 
 const page = () => {
   return (
-    <div className="flex min-w-full min-h-screen justify-center items-center">
-      <h1 className="text-white font-bold text-4xl">
-        Clothing Section Coming Soon...
-      </h1>
+    <div className="flex min-w-full min-h-screen">
+      <div className="container mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-32 place-items-center gap-8">
+        {clothingData.map((item) => (
+          <ClothingDisplay item={item} key={item.id} />
+        ))}
+      </div>
     </div>
   );
 };
--- components/ClothingDisplay.tsx
+++ components/ClothingDisplay.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-const ClothingDisplay = () => {
+import Image from "next/image";
+
+import clothingData from "@/dataSets/ClothingData";
+
+type ClothingItem = {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  discountPrice: number;
+  description: string;
+  sizes: string[];
+  image: string;
+};
+
+type ClothingProps = {
+  item: ClothingItem;
+};
+
+const ClothingDisplay = ({ item }: ClothingProps) => {
   return (
-    <div>ClothingDisplay</div>
-  )
-}
+    <Card className="w-[300px] lg-h-[350px] bg-white border-4 border-amber-700" key={item.id}>
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle className="text-lg  text-teal-800 font-extrabold">{item.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="relative w-[200px] h-[200px] rounded-lg">
+            <Image src={"/images/ClothingTrezy.jpg"} alt="Item Image" fill={true} className="rounded-lg"></Image>
+          </div>
 
-export default ClothingDisplay
+          <p className="text-md font-semibold text-gray-600">
+            {item.description}
+          </p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ClothingDisplay;
